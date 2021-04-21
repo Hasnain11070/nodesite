7fca219fdd47f8f501e9540504931c64c56284e3
Hasnain11070 <77341190+Hasnain11070@users.noreply.github.com>
2021-04-21 22:19:57 +0500

    mynodesite

---
+++ src/models/usermessage.js
@@ -0,0 +1,35 @@
+const { data } = require("jquery");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email");
+            }
+        }
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minLength: 10
+    },
+    message: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+const User = mongoose.model("User", userSchema);
+module.exports = User;
